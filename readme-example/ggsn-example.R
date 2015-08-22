12365af5a282a5f9bb51d140990ea15a00eea020
oswaldo <oswaldosant@gmail.com>
2015-08-22 10:27:49 -0300

    Update

--- readme-example/ggsn-example.R
+++ readme-example/ggsn-example.R
@@ -1,10 +1,17 @@
-library(ggsn); library(ggmap)
-library(rgdal); library(rgeos)
-dsn <- system.file('extdata', package = 'ggsn')
-map <- readOGR(dsn, 'sp')
+library(ggsn); library(ggmap); library(rgdal); library(rgeos)
+shape.directory <- system.file('extdata', package = 'ggsn')
+
+# Map in geographic coordinates
+map <- readOGR(shape.directory, 'sp')
 map@data$id <- 1:nrow(map@data)
 map.ff <- fortify(map, region = 'id')
 map.df <- merge(map.ff, map@data, by = 'id')
+
+# Map in projected coordinates
+map2 <- spTransform(map, CRS("+init=epsg:31983"))
+map2@data$id <- 1:nrow(map2@data)
+map2.ff <- fortify(map2, region = 'id')
+map2.df <- merge(map2.ff, map2@data, by = 'id')
 
 tiff('north-symbols.tiff', width = 6.83, height = 6.83,
      units = 'in', compression = 'lzw', res = 600)
@@ -13,36 +20,22 @@ dev.off()
 
 tiff('map1.tiff', width = 6.83, height = 6.83,
      units = 'in', compression = 'lzw', res = 600)
-ggplot(data = map.df, aes(long, lat, group = group, fill = nots)) +
+(ggm1 <- ggplot(data = map.df, aes(long, lat, group = group, fill = nots)) +
     geom_polygon() +
     coord_equal() +
     geom_path() +
-    blank() +
-    north(map.df) +
-    scale_fill_brewer(name = 'Animal abuse\nnotifications', palette = 8) +
-    scalebar(map.df, dist = 5, dd2km = TRUE, model = 'WGS84', st.size = 4)
+    scale_fill_brewer(name = 'Animal abuse\nnotifications', palette = 8))
 dev.off()
 
 tiff('map2.tiff', width = 6.83, height = 6.83,
      units = 'in', compression = 'lzw', res = 600)
-ggplot(data = map.df, aes(long, lat, group = group, fill = nots)) +
-    geom_polygon() +
-    coord_equal() +
-    geom_path() +
-    north(map.df, symbol = 14) +
-    scale_fill_brewer(name = 'Animal abuse\nnotifications', palette = 8) +
+ggm1 +
+    blank() +
+    north(map.df) +
     scalebar(map.df, dist = 5, dd2km = TRUE, model = 'WGS84', st.size = 4)
 dev.off()
 
 tiff('map3.tiff', width = 6.83, height = 6.83,
-     units = 'in', compression = 'lzw', res = 600)
-map2 <- spTransform(map, CRS("+init=epsg:31983"))
-map2@data$id <- 1:nrow(map2@data)
-map2.ff <- fortify(map2, region = 'id')
-map2.df <- merge(map2.ff, map2@data, by = 'id')
-dev.off()
-
-tiff('map4.tiff', width = 6.83, height = 6.83,
      units = 'in', compression = 'lzw', res = 600)
 ggplot(data = map2.df, aes(long, lat, group = group, fill = nots)) +
     geom_polygon() +
@@ -58,7 +51,7 @@ dev.off()
 sp <- get_map(bbox(map) * matrix(rep(c(1.001, 0.999), e = 2), ncol = 2),
               source = 'osm')
 
-tiff('map5.tiff', width = 6.83, height = 6.83,
+tiff('map4.tiff', width = 6.83, height = 6.83,
      units = 'in', compression = 'lzw', res = 600)
 ggmap(sp, extent = 'device') +
     geom_polygon(data = map.df, aes(long, lat, group = group, fill = nots),
