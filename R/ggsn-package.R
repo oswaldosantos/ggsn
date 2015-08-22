3f01af203ce16f39c6628fb4170d89368c60c214
oswaldo <oswaldosant@gmail.com>
2015-08-22 15:46:22 -0300

    Update DESCRIPTION and Imports

--- R/ggsn-package.R
+++ R/ggsn-package.R
@@ -1,13 +1,13 @@
-#' North symbols and scale bars for maps created with ggplot or ggmap. Adds north symbols (18 options) and scale bars in kilometers to maps in geographic or metric coordinates created with ggplot or ggmap.
+#' North symbols and scale bars for maps created with 'ggplot' or 'ggmap'. Adds north symbols (18 options) and scale bars in kilometers to maps in geographic or metric coordinates created with 'ggplot' or 'ggmap'.
 #'
 #' \tabular{ll}{
 #' Package: \tab ggsn\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.1.1\cr
-#' Date: \tab 2015-08-21\cr
-#' Depends: \tab R (>= 3.2.1)\cr
+#' Version: \tab 0.2.0\cr
+#' Date: \tab 2015-08-22\cr
+#' Depends: \tab R (>= 3.2.1), ggplot2\cr
 #' Imports: \tab png, grid, maptools\cr
-#' Suggests: \tab rgdal, ggmap\cr
+#' Suggests: \tab rgdal, ggmap, rgeos\cr
 #' License: \tab GPL (>= 2)\cr
 #' LazyLoad: \tab yes\cr
 #' URL: \tab \url{https://github.com/oswaldosantos/ggsn}\cr
@@ -20,4 +20,5 @@
 #' @title The ggsn Package
 #' @keywords package
 #' @import ggplot2 png grid maptools
+#' @importFrom graphics plot rasterImage
 NULL
